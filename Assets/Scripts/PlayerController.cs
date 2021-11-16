0d18d8d8d82e188ae1c9bf35465dc8ee9331ca52
guilloClaus <guillermo.j.ojeda.developer@gmail.com>
2021-11-15 22:25:25 -0300

    intento de animacion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private float velocidadPlayer = 1000f;
     [SerializeField] private float fuerzaSalto = 500f;
-    [SerializeField] private float velocidadGiro = 10f;    
+    [SerializeField] private float velocidadGiro = 10f;
     [SerializeField] LayerMask groundLayer;
     [SerializeField] private Animator animaPlayer;
 
@@ -29,54 +29,105 @@ public class PlayerController : MonoBehaviour
     {
         rbPlayer = GetComponent<Rigidbody>();
 
+        animaPlayer.SetBool("IsIdle", true);
+        animaPlayer.SetBool("IsRun", false);
+        animaPlayer.SetBool("IsWalkBack", false);
+        animaPlayer.SetBool("IsJump", false);
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-
+        //isGrounded = IsGrounded();
     }
 
     private void FixedUpdate()
     {
-        if (isGrounded)
-            Mover();
-    }
+
+        
+        Mover();
+        ControlAnimacion();
+    }   
 
     private void Mover()
     {
-
-        float ejeHorizontal = Input.GetAxis("Horizontal");
         float ejeVertical = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && isGrounded)
         {
             giroPlayer -= Time.deltaTime * velocidadGiro * 10;
             transform.rotation = Quaternion.Euler(0, giroPlayer, 0);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D) && isGrounded)
         {
             giroPlayer += Time.deltaTime * velocidadGiro * 10;
             transform.rotation = Quaternion.Euler(0, giroPlayer, 0);
         }
 
 
-        if (ejeHorizontal != 0 || ejeVertical != 0)
+        if (ejeVertical != 0 && isGrounded)
         {
             rbPlayer.AddRelativeForce(Vector3.forward * velocidadPlayer * ejeVertical, ForceMode.Force);
-         //   rbPlayer.AddRelativeForce(Vector3.right * velocidadPlayer * ejeHorizontal, ForceMode.Force);
+            //transform.Translate(velocidadPlayer * Time.deltaTime * new Vector3(0, 0, ejeVertical));
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
             rbPlayer.AddRelativeForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+        }
+
+
+
     }
+
+    private void ControlAnimacion()
+    {
+
+        float ejeVertical = Input.GetAxis("Vertical");
+        bool salto = Input.GetKey(KeyCode.Space);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        { 
+            animaPlayer.SetBool("IsWalkBack", true);
+        }
+        else
+        {
+            animaPlayer.SetBool("IsWalkBack", false);
+        }
+        
+        if (ejeVertical != 0)
+        {
+            animaPlayer.SetBool("IsRun", ejeVertical > 0);
+            animaPlayer.SetBool("IsWalkBack", ejeVertical < 0);
+        }
+        else
+        {
+            animaPlayer.SetBool("IsRun", false);
+        }
+
+        if (salto)
+            animaPlayer.SetBool("IsJump", true);
+        else
+            animaPlayer.SetBool("IsJump", false);
+
+
+        animaPlayer.SetBool("IsIdle", ejeVertical == 0 && !salto && !Input.GetKey(KeyCode.A) || !Input.GetKey(KeyCode.D));
+
+
+        Debug.Log($"IsIdle {animaPlayer.GetBool("IsIdle")} ; IsJump {animaPlayer.GetBool("IsJump")}; IsRun {animaPlayer.GetBool("IsRun")} ; IsBack {animaPlayer.GetBool("IsWalkBack")}; giroPlayer {giroPlayer}; isGround {isGrounded}");
+
+
+
+    }
+
+
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == 6)
         {
-
             isGrounded = true;
         }
     }
@@ -85,22 +136,18 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.layer == 6)
         {
-
             isGrounded = false;
         }
     }
 
     //private bool IsGrounded()
     //{
-    //    if (Physics.Raycast(transform.position, Vector3.down, 0.01f, groundLayer))
-    //    {
-    //        Debug.Log("SI el Piso");
+    //    if (Physics.Raycast(transform.position, Vector3.down, 0.05f, groundLayer))
+    //    {         
     //        return true;
-
     //    }
     //    else
     //    {
-    //        Debug.Log("NO el Piso");
     //        return false;
     //    }
     //}
