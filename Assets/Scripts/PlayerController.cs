1158aea584369aa641b1c54a5e96f8719cb0484d
guilloClaus <guillermo.j.ojeda.developer@gmail.com>
2021-11-17 01:15:00 -0300

    Correccion de animaciones y movimiento

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,12 +12,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Animator animaPlayer;
 
     private bool isGrounded = true;
+    private bool isRotate = false;
+    private bool isWalk = false;
+
     private float giroPlayer = 0f;
-
-
-    private const string HORIZONTAL_AXIS = "Horizontal";
-    private const string VERTICAL_AXIS = "Vertical";
-
 
     //[SerializeField] private Animator animaPlayer = new Animator();
 
@@ -28,6 +26,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody>();
+
 
         animaPlayer.SetBool("IsIdle", true);
         animaPlayer.SetBool("IsRun", false);
@@ -45,11 +44,9 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-
-        
         Mover();
         ControlAnimacion();
-    }   
+    }
 
     private void Mover()
     {
@@ -59,64 +56,52 @@ public class PlayerController : MonoBehaviour
         {
             giroPlayer -= Time.deltaTime * velocidadGiro * 10;
             transform.rotation = Quaternion.Euler(0, giroPlayer, 0);
+            isRotate = true;
         }
         else if (Input.GetKey(KeyCode.D) && isGrounded)
         {
             giroPlayer += Time.deltaTime * velocidadGiro * 10;
             transform.rotation = Quaternion.Euler(0, giroPlayer, 0);
+            isRotate = true;
+        }
+        else
+        {
+            isRotate = false;
         }
 
+        if (Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        {
+            isWalk = true;
+            rbPlayer.AddRelativeForce(Vector3.forward * (velocidadPlayer * 0.91f), ForceMode.Force);
+        }
+        else { isWalk = false; }
 
-        if (ejeVertical != 0 && isGrounded)
+        if (ejeVertical != 0 && isGrounded && !isWalk)
         {
             rbPlayer.AddRelativeForce(Vector3.forward * velocidadPlayer * ejeVertical, ForceMode.Force);
-            //transform.Translate(velocidadPlayer * Time.deltaTime * new Vector3(0, 0, ejeVertical));
         }
 
         if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
             rbPlayer.AddRelativeForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
         }
-
-
-
     }
 
     private void ControlAnimacion()
     {
 
         float ejeVertical = Input.GetAxis("Vertical");
-        bool salto = Input.GetKey(KeyCode.Space);
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
-        { 
-            animaPlayer.SetBool("IsWalkBack", true);
-        }
-        else
-        {
-            animaPlayer.SetBool("IsWalkBack", false);
-        }
-        
-        if (ejeVertical != 0)
-        {
-            animaPlayer.SetBool("IsRun", ejeVertical > 0);
-            animaPlayer.SetBool("IsWalkBack", ejeVertical < 0);
-        }
-        else
-        {
-            animaPlayer.SetBool("IsRun", false);
-        }
-
-        if (salto)
-            animaPlayer.SetBool("IsJump", true);
-        else
-            animaPlayer.SetBool("IsJump", false);
+        animaPlayer.SetBool("IsWalk", Input.GetKey(KeyCode.LeftShift));
+        animaPlayer.SetBool("IsRun", ejeVertical > 0);
+        animaPlayer.SetBool("IsWalkBack", ejeVertical < 0 || isRotate);
+        animaPlayer.SetBool("IsJump", !isGrounded);
 
 
-        animaPlayer.SetBool("IsIdle", ejeVertical == 0 && !salto && !Input.GetKey(KeyCode.A) || !Input.GetKey(KeyCode.D));
+        animaPlayer.SetBool("IsIdle", ejeVertical == 0 && !salto && !isRotate && !Input.GetKey(KeyCode.LeftShift));
 
 
-        Debug.Log($"IsIdle {animaPlayer.GetBool("IsIdle")} ; IsJump {animaPlayer.GetBool("IsJump")}; IsRun {animaPlayer.GetBool("IsRun")} ; IsBack {animaPlayer.GetBool("IsWalkBack")}; giroPlayer {giroPlayer}; isGround {isGrounded}");
+        Debug.Log($"IsWalk {animaPlayer.GetBool("IsWalk")}; IsIdle {animaPlayer.GetBool("IsIdle")} ; IsJump {animaPlayer.GetBool("IsJump")}; IsRun {animaPlayer.GetBool("IsRun")} ; IsWalkBack {animaPlayer.GetBool("IsWalkBack")}; giroPlayer {giroPlayer}; isGround {isGrounded}");
 
 
 
